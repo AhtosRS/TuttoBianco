add85a982455c5ab86657db7ad880ce3d581ec03
AhtosRS <felipele89@gmail.com>
2022-02-05 13:28:46 -0300

    navbar

---
+++ src/components/NavBar.js
@@ -0,0 +1,22 @@
+import react from "react";
+
+function Navigation() {
+    return( <div className="navContainer">
+        <div className="branding">
+            <p>Tutto-Bianco</p>
+        </div>
+        <div className="products">
+           <ul className="list">
+                <li className="item"><a href="#">Remeras</a></li>
+                <li className="item"><a href="#">Pantalones</a></li>
+                <li className="item"><a href="#">Zapatillas</a></li>
+                <li className="item"><a href="#">Accesorios</a></li>
+            </ul> 
+        </div>
+        
+    </div>
+
+    )
+}
+
+export default Navigation;
